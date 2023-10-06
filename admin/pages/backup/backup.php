4b399f5977ebd03635d156aec0efac991bf365b3
Student1220517 <79580131+Student1220517@users.noreply.github.com>
2023-10-06 22:29:26 +0700

    Update backup.php
    hoàn thành chức năng backup dữ liệu

--- admin/pages/backup/backup.php
+++ admin/pages/backup/backup.php
@@ -1,15 +1,17 @@
 <?php
-// include("../../../mod/database_connection.php");
+include("../../../mod/database_connection.php");
+$backupFileName = "backup_" . date("Y-m-d_H-i-s") . ".sql";
+$backupFilePath = "C:\\xampp\\htdocs\\Galaxy_Game_Store\\admin\\pages\\backup\\" . $backupFileName;
+$command = "mysqldump --user=root --host=127.0.0.1 --password= galaxy_game_store > $backupFilePath";
 
-// $command = "mysqldump --user=root --host=127.0.0.1 -p galaxy_game_store > backup.sql";
-// exec($command, $output, $returnVar);
+exec($command, $output, $returnVar);
 
-// if ($returnVar === 0) {
-//     echo "Database backup successful.";
-// } else {
-//     echo "Database backup failure1: " . implode("\n", $output);
-// }
+if ($returnVar === 0) {
+    echo "Database backup successful.";
+} else {
+    echo "Database backup failure";
+}
 
-// $conn->close();
+$conn->close();
 
-?>
+?> 
